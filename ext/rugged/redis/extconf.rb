410f062a3178e2cfc68c53cae6bf8717f973cb3e
Viktor Charypar <charypar@gmail.com>
2014-04-14 17:10:21 +0100

    Basic structure using rake-compiler

---
+++ ext/rugged/redis/extconf.rb
@@ -0,0 +1,8 @@
+require "mkmf"
+
+# TODO build redis from libgit2-backends using
+# https://github.com/libgit2/rugged/blob/development/ext/rugged/extconf.rb
+# as a guide
+
+# and then
+# create_makefile "rugged/redis"
